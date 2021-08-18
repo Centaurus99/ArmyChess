8daf3295cec8df69809266b66c16f62a5926933d
Centaurus99 <1102569568@qq.com>
2021-08-18 22:37:39 +0800

    Setting up the chessboard

--- src/framework/game.cpp
+++ src/framework/game.cpp
@@ -1,4 +1,106 @@
 #include "game.h"
+#include <iostream>
+#include <vector>
+
+// The number of safe nodes in the upper half
+std::vector<int> SAFE_NODE = { 11, 13, 17, 21, 23 };
+
+// Get number of node from Coordinate
+inline int Index(const int& x, const int& y) { return 5 * x + y; }
+
+// Coordinate change list used in the BFS
+int dx[] = { -1, 0, 0, 1, -1, -1, 1, 1 };
+int dy[] = { 0, -1, 1, 0, -1, 1, -1, 1 };
 
 Game::Game() {
+    // Set safe nodes
+    for (auto x : SAFE_NODE) {
+        nodes[x].setSafe(true);
+        nodes[x + 30].setSafe(true);
+    }
+
+    // Set coordinate as follows
+    //  0,  1, ...,  4
+    //  :   :        :
+    // 25, 26, ..., 29
+    // ---------------
+    // 59, 58, ..., 55
+    //  :   :        :
+    // 34, 33, ..., 30
+    for (int i = 0; i < 6; ++i) {
+        for (int j = 0; j < 5; ++j) {
+            index[i][j] = Index(i, j);
+            index[11 - i][4 - j] = Index(i, j) + 30;
+            nodes[Index(i, j)].setCoordinate(i, j);
+            nodes[Index(i, j) + 30].setCoordinate(11 - i, 4 - j);
+        }
+    }
+
+    // Set edge
+    for (int j = 0; j < 5; ++j) {
+        if (j != 4) {
+            nodes[index[0][j]].road.push_back(index[0][j + 1]);
+            nodes[index[0][j] + 30].road.push_back(index[0][j + 1] + 30);
+            nodes[index[1][j]].railway_horizontal.push_back(index[1][j + 1]);
+            nodes[index[1][j] + 30].railway_horizontal.push_back(index[1][j + 1] + 30);
+            nodes[index[5][j]].railway_horizontal.push_back(index[5][j + 1]);
+            nodes[index[5][j] + 30].railway_horizontal.push_back(index[5][j + 1] + 30);
+        }
+        if (j != 0) {
+            nodes[index[0][j]].road.push_back(index[0][j - 1]);
+            nodes[index[0][j] + 30].road.push_back(index[0][j - 1] + 30);
+            nodes[index[1][j]].railway_horizontal.push_back(index[1][j - 1]);
+            nodes[index[1][j] + 30].railway_horizontal.push_back(index[1][j - 1] + 30);
+            nodes[index[5][j]].railway_horizontal.push_back(index[5][j - 1]);
+            nodes[index[5][j] + 30].railway_horizontal.push_back(index[5][j - 1] + 30);
+        }
+        nodes[index[0][j]].road.push_back(index[1][j]);
+        nodes[index[0][j] + 30].road.push_back(index[1][j] + 30);
+        nodes[index[1][j]].road.push_back(index[0][j]);
+        nodes[index[1][j] + 30].road.push_back(index[0][j] + 30);
+    }
+    for (int i = 1; i < 6; ++i) {
+        if (i != 5) {
+            nodes[index[i][0]].railway_vertical.push_back(index[i + 1][0]);
+            nodes[index[i][0] + 30].railway_vertical.push_back(index[i + 1][0] + 30);
+            nodes[index[i][4]].railway_vertical.push_back(index[i + 1][4]);
+            nodes[index[i][4] + 30].railway_vertical.push_back(index[i + 1][4] + 30);
+        }
+        if (i != 1) {
+            nodes[index[i][0]].railway_vertical.push_back(index[i - 1][0]);
+            nodes[index[i][0] + 30].railway_vertical.push_back(index[i - 1][0] + 30);
+            nodes[index[i][4]].railway_vertical.push_back(index[i - 1][4]);
+            nodes[index[i][4] + 30].railway_vertical.push_back(index[i - 1][4] + 30);
+        }
+    }
+    for (int j = 0; j < 5; j += 2) {
+        nodes[index[5][j]].railway_vertical.push_back(index[6][j]);
+        nodes[index[6][j]].railway_vertical.push_back(index[5][j]);
+    }
+    for (int i = 2; i <= 4; ++i) {
+        for (int j = 1; j <= 3; ++j) {
+            int lim = nodes[index[i][j]].isSafe() ? 8 : 4;
+            for (int k = 0; k < lim; ++k) {
+                int x = i + dx[k], y = j + dy[k];
+                nodes[index[i][j]].road.push_back(index[x][y]);
+                nodes[index[i][j] + 30].road.push_back(index[x][y] + 30);
+                if (!(2 <= x && x <= 4 && 1 <= y && y <= 3)) {
+                    nodes[index[x][y]].road.push_back(index[i][j]);
+                    nodes[index[x][y] + 30].road.push_back(index[i][j] + 30);
+                }
+            }
+        }
+    }
+}
+
+int main() {
+    Game g;
+    freopen("log.log", "w", stdout);
+    for (int i = 0; i < 60; ++i) {
+        std::cout << i << ':';
+        for (auto x : g.nodes[i].railway_horizontal) {
+            std::cout << x << ' ';
+        }
+        std::cout << std::endl;
+    }
 }
--- src/framework/game.h
+++ src/framework/game.h
@@ -1,8 +1,43 @@
 #ifndef GAME_H
 #define GAME_H
 
-class Game {
+#include <vector>
+
+class Node {
+private:
+    int x_, y_; // coordinate
+    int type_; // the type of chess on this node
+    bool safe_; // whether this node is safe
+
 public:
+    std::vector<int> road; // nodes linked with road
+    std::vector<int> railway_vertical; // nodes linked with vertical railway
+    std::vector<int> railway_horizontal; // nodes linked with horizontal railway
+
+    /// @param x Row number
+    /// @param y Column number
+    /// @param type Chess type
+    /// @param safe Whether this node is safe
+    Node(const int& x = 0, const int& y = 0, const int& type = 0, const bool& safe = 0)
+        : x_ { x }
+        , y_ { y }
+        , type_ { type }
+        , safe_ { safe } { }
+
+    Node(const Node& other) = delete;
+
+    void setCoordinate(const int& x, const int& y) { x_ = x, y_ = y_; }
+    void setType(const int& type) { type_ = type; }
+    void setSafe(const bool& safe) { safe_ = safe; }
+
+    bool isSafe() { return safe_; }
+};
+
+class Game {
+private:
+public:
+    Node nodes[60]; // Nodes list
+    int index[12][5]; // Number of node from Coordinate
     Game();
 };
 
