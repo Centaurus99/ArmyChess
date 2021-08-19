60c20460b8c72dbe384f0e5fed935581e4603fcb
Centaurus99 <1102569568@qq.com>
2021-08-20 00:43:52 +0800

    Set chessboard

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,12 +1,54 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QtWidgets/QPushButton>
 
 MainWindow::MainWindow(QWidget* parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow) {
     ui->setupUi(this);
+
+    QPushButton* pushButton = new QPushButton(ui->centerFrame);
+    pushButton->setGeometry(1, 1, 10, 10);
+    //    ui->label->setPixmap(QPixmap(":/image/png/chessboard1.png"));
+    //    ui->label->setScaledContents(true);
+    //    ui->label->setText("");
 }
 
-MainWindow::~MainWindow() {
-    delete ui;
+MainWindow::~MainWindow() { delete ui; }
+
+void MainWindow::resizeEvent(QResizeEvent* event) {
+
+    ui->horizontalSpacer->changeSize(0, 0);
+    ui->horizontalSpacer_2->changeSize(0, 0);
+    ui->verticalSpacer->changeSize(0, 0);
+    ui->verticalSpacer_2->changeSize(0, 0);
+
+    float remaining_width = ui->centralwidget->width() - border_width * 2.0;
+    float remaining_height = ui->centralwidget->height() - border_width * 2.0;
+
+    //    qDebug() << remaining_width << ':' << remaining_height;
+
+    if (remaining_height <= remaining_width * HW_ratio) {
+        // Height is not enough
+        int delta
+            = round((remaining_width - remaining_height / HW_ratio) / 2.0);
+        qDebug() << delta;
+        ui->horizontalSpacer->changeSize(delta, 0);
+        ui->horizontalSpacer_2->changeSize(delta, 0);
+    } else {
+        // Width is not enough
+        int delta
+            = round((remaining_height - remaining_width * HW_ratio) / 2.0);
+        qDebug() << delta;
+        ui->verticalSpacer->changeSize(0, delta);
+        ui->verticalSpacer_2->changeSize(0, delta);
+    }
+
+    ui->horizontalLayout->update();
+    ui->verticalLayout->update();
+
+    //    qDebug() << ui->centerFrame->width() << ':' <<
+    //    ui->centerFrame->height(); qDebug() << ui->centralwidget->width() <<
+    //    ':' << ui->centralwidget->height()
+    //             << '\n';
 }
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -16,7 +16,15 @@ public:
     MainWindow(QWidget* parent = nullptr);
     ~MainWindow();
 
+    // Set chessboard in the center with a specific aspect ratio
+    void resizeEvent(QResizeEvent* event);
+
 private:
     Ui::MainWindow* ui;
+
+    // The aspect ratio of the chessboard
+    const float HW_ratio = 924.0 / 651.0;
+    // Width of border around centerFrame
+    const int border_width = 9;
 };
 #endif // MAINWINDOW_H
