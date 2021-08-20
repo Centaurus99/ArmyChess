dda602d8415603daf17dbb80ec052a1236232ab0
Centaurus99 <1102569568@qq.com>
2021-08-20 12:25:56 +0800

    Improve the implementation of the ChessButton

--- src/widget/chessbutton.cpp
+++ src/widget/chessbutton.cpp
@@ -1,38 +1,33 @@
 #include "chessbutton.h"
 #include <QGraphicsDropShadowEffect>
 
-ChessButton::ChessButton(QWidget* parent)
+void ChessButton::loadIcon(const QString& res_name) { icon_.load(res_name); }
+
+void ChessButton::setIcon() {
+    QIcon icon;
+    icon.addPixmap(icon_.scaled(width() - 8, height() - 8), QIcon::Normal);
+    icon.addPixmap(icon_.scaled(width() - 8, height() - 8), QIcon::Disabled);
+    QPushButton::setIcon(icon);
+    setIconSize(size());
+}
+
+ChessButton::ChessButton(QWidget* parent, const QString& icon_name)
     : QPushButton { parent } {
 
+    // Set shadow
     QGraphicsDropShadowEffect* effect = new QGraphicsDropShadowEffect;
-    effect->setBlurRadius(10); // 阴影圆角的大小
-    effect->setColor(Qt::black); //阴影的颜色
+    effect->setBlurRadius(10);
+    effect->setColor(Qt::black);
     effect->setOffset(0, 3);
-    //    QPushButton* this = new QPushButton(ui->centerFrame);
+    setGraphicsEffect(effect);
 
-    this->setCursor(QCursor(Qt::PointingHandCursor));
-    this->setGraphicsEffect(effect);
-    this->setGeometry(20, 20, 100, 100);
-    this->setFlat(true);
-    /*    QPixmap icon(":/image/png/vacant_chess.png");
-        qDebug() << icon.width() << ':' << icon.height();
-        icon = icon.scaled(this->width() - 10, this->height() - 10);
-        qDebug() << icon.width() << ':' << icon.height();*/
+    // Set Cursor
+    setCursor(QCursor(Qt::PointingHandCursor));
 
-    QIcon icon;
+    // Make it Transparent
+    setFlat(true);
 
-    icon.addPixmap(QPixmap(":/image/png/vacant_chess.png")
-                       .scaled(this->width() - 8, this->height() - 8),
-        QIcon::Normal);
-    icon.addPixmap(QPixmap(":/image/png/vacant_chess.png")
-                       .scaled(this->width() - 8, this->height() - 8),
-        QIcon::Disabled);
-    this->setIcon(icon);
-    this->setIconSize(this->size());
-    //    this->setEnabled(false);
-    /*this->setStyleSheet(
-        "QPushButton{border-image: url(:/image/png/vacant_chess.png);}"
-        "QPushButton:hover{border-image: url(:/image/png/vacant_node.png);}"
-        "QPushButton:pressed{border-image: "
-        "url(:/image/png/vacant_camp.png);}");*/
+    // Set icon
+    loadIcon(icon_name);
+    setIcon();
 }
--- src/widget/chessbutton.h
+++ src/widget/chessbutton.h
@@ -4,8 +4,19 @@
 #include <QPushButton>
 
 class ChessButton : public QPushButton {
+private:
+    // Button icon pixmap
+    QPixmap icon_;
+
 public:
-    explicit ChessButton(QWidget* parent = nullptr);
+    /// @param parent parent widget
+    /// @param icon_name default icon
+    ChessButton(QWidget* parent = nullptr, const QString& icon_name = "");
+
+    // Load icon from resource
+    void loadIcon(const QString& res_name);
+    // Set icon according to button size
+    void setIcon();
 };
 
 #endif // CHESSBUTTON_H
